535e20aa505385b89638e69fbc13ea697e0b7bfe
Moreno Razzoli <morrolinux@gmail.com>
2021-07-27 16:21:30 +0200

    animation view

--- 3dbox_to_img.py
+++ 3dbox_to_img.py
@@ -1,31 +1,79 @@
+import argparse
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import math
 
 from skimage import io
 from matplotlib.lines import Line2D
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--file_id", default='000010')
+args = parser.parse_args()
+
 colors = sns.color_palette('Paired', 9 * 2)
 names = ['Car', 'Van', 'Truck', 'Pedestrian', 'Person_sitting', 'Cyclist', 'Tram', 'Misc', 'DontCare']
-file_id = '000010'
 
-if __name__ == '__main__':
+file_id = args.file_id
 
-  # load image
-  img = np.array(io.imread(f'examples\\kitti\\image_2\\{file_id}.png'), dtype=np.int32)
 
-  # load labels
-  with open(f'examples\\kitti\\label_2\\{file_id}.txt', 'r') as f:
-    labels = f.readlines()
+# Calculates rotation matrix to euler angles
+# The result is the same as MATLAB except the order
+# of the euler angles ( x and z are swapped ).
+def rotationMatrixToEulerAngles(R) :
+    yaw=math.atan2(R[1,0], R[0,0])
+    pitch=math.atan2(-R[2,0], math.sqrt(R[2,1]**2+R[2,2]**2))
+    roll=math.atan2(R[2,1], R[2,2])
+    return pitch, yaw, roll
 
-  # load calibration file
-  with open(f'examples\\kitti\\calib\\{file_id}.txt', 'r') as f:
-    lines = f.readlines()
-    P2 = np.array(lines[2].strip().split(' ')[1:], dtype=np.float32).reshape(3, 4)
+# Calculates Rotation Matrix given euler angles.
+def eulerAnglesToRotationMatrix(theta) :
 
-  fig = plt.figure()
+    R_x = np.array([[1,         0,                  0                   ],
+                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
+                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
+                    ])
+
+    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
+                    [0,                     1,      0                   ],
+                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
+                    ])
+
+    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
+                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
+                    [0,                     0,                      1]
+                    ])
+
+    R = np.dot(R_z, np.dot( R_y, R_x ))
+
+    return R
+
+# Calculates projection matrix given intrinsics and extrinsic mat
+def calc_P(intrinsic_mat, extrinsic_mat, offset=0):
+    ravel_mode = 'C'
+    P0 = intrinsic_mat
+    P0 = np.column_stack((P0, np.array([0, 0, 0])))
+
+    R = extrinsic_mat[:3, :3]
+    T = extrinsic_mat[:3, 3]
+    T1 = np.array([0, T[1], 10])   # x (carla -y), y (carla -z), z (carla x),  
+
+    pitch, yaw, roll = rotationMatrixToEulerAngles(R)
+
+    R1 = np.array(eulerAnglesToRotationMatrix((offset, 0, 0)))  # roll (carla pitch), pitch (carla yaw), img_yaw (carla roll)
+    
+    RT1 = np.column_stack((R1, T1))
+    RT1 = np.row_stack((RT1, np.array([0, 0, 0, 1])))
+    print("RT1:", RT1)
+
+    P0 = np.matmul(P0, RT1)
+
+    return P0
+
+
+def draw_labels(ax, labels, P2, pause=0.001):
   # draw image
-  plt.imshow(img)
+  # plt.imshow(img)
 
   for line in labels:
     line = line.split()
@@ -50,7 +98,7 @@ if __name__ == '__main__':
 
 
       def line(p1, p2, front=1):
-        plt.gca().add_line(Line2D((p1[0], p2[0]), (p1[1], p2[1]), color=colors[names.index(lab) * 2 + front]))
+        ax.add_line(Line2D((p1[0], p2[0]), (p1[1], p2[1]), color=colors[names.index(lab) * 2 + front]))
 
 
       # draw the upper 4 horizontal lines
@@ -72,7 +120,45 @@ if __name__ == '__main__':
       line(corners_img[7], corners_img[3])
 
   # fig.patch.set_visible(False)
+  # plt.savefig('examples/kitti_3dbox_to_img.png', bbox_inches='tight')
+  fig.canvas.draw()
+  plt.pause(pause)
+  ax.lines.clear()
+  # plt.close(fig)
+  # plt.show()
+
+
+if __name__ == '__main__':
+
+  # load image
+  img = np.array(io.imread(f'examples/kitti/image_2/{file_id}.png'), dtype=np.int32)
+
+  # load labels
+  with open(f'examples/kitti/label_2/{file_id}.txt', 'r') as f:
+    labels = f.readlines()
+
+  # load intrinsics and extrinsics
+  with open(f'examples/kitti/calib/{file_id}.i', 'rb') as f:
+    intrinsic_mat = np.load(f)
+  with open(f'examples/kitti/calib/{file_id}.e', 'rb') as f:
+    extrinsic_mat = np.load(f)
+
+  # load calibration file
+  # with open(f'examples/kitti/calib/{file_id}.txt', 'r') as f:
+  #   lines = f.readlines()
+  #   P2 = np.array(lines[2].strip().split(' ')[1:], dtype=np.float32).reshape(3, 4)
+
+  fig = plt.figure()
+  ax = fig.gca()
+  fig.show()
+  ax.imshow(img)
   plt.axis('off')
   plt.tight_layout()
-  plt.savefig('examples/kitti_3dbox_to_img.png', bbox_inches='tight')
-  plt.show()
+  figManager = plt.get_current_fig_manager()
+  figManager.window.showMaximized()
+
+  # P2 = calc_P(intrinsic_mat, extrinsic_mat)
+  # draw_labels(labels, P2)
+
+  for i in range(100):
+    draw_labels(ax, labels, calc_P(intrinsic_mat, extrinsic_mat, offset=i/100))
---
+++ view_last.sh
@@ -0,0 +1,1 @@
+python 3dbox_to_img.py --file_id $(ls ~/Carla0.9.10-kitti-data-export/_out/training/image_2/|sort|tail -n1|cut -d. -f1)
