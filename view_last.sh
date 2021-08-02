65c8669dacac64aba31b4ecb3295ab9da0b6df7a
Moreno Razzoli <morrolinux@gmail.com>
2021-08-02 09:46:47 +0200

    specify offset to see the n-th last element like so: -n

--- view_last.sh
+++ view_last.sh
@@ -1,1 +1,3 @@
-python 3dbox_to_img.py --file_id $(ls ~/Carla0.9.10-kitti-data-export/_out/training/image_2/|sort|tail -n1|cut -d. -f1)
+num=$(ls ~/Carla0.9.10-kitti-data-export/_out/training/image_2/|sort|tail -n1|cut -d. -f1)
+num=$(printf "%06d" $(($num+$1)))
+python 3dbox_to_img.py --file_id $num
