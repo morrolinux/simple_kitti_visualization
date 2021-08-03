5e8b3ea73f2b6a51725d18e8e35dfb7d07d9e633
Moreno Razzoli <morrolinux@gmail.com>
2021-08-03 11:13:19 +0200

    fix offset logic

--- view_last.sh
+++ view_last.sh
@@ -1,3 +1,5 @@
 num=$(ls ~/Carla0.9.10-kitti-data-export/_out/training/image_2/|sort|tail -n1|cut -d. -f1)
-num=$(printf "%06d" $(($num+$1)))
+if [[ $1 -eq 0 ]]; then nth=0; else nth=$1;fi
+num=$(printf "%06d" $(echo $num + nth| bc))
+echo SHOWING $num
 python 3dbox_to_img.py --file_id $num
