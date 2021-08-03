59567a23fb0008cc6325772b3ec5560dada07ee1
Moreno Razzoli <morrolinux@gmail.com>
2021-08-03 12:20:18 +0200

    fix bug

--- view_last.sh
+++ view_last.sh
@@ -1,5 +1,6 @@
 num=$(ls ~/Carla0.9.10-kitti-data-export/_out/training/image_2/|sort|tail -n1|cut -d. -f1)
-if [[ $1 -eq 0 ]]; then nth=0; else nth=$1;fi
-num=$(printf "%06d" $(echo $num + nth| bc))
+if [[ -z $1 ]]; then nth=0; else nth=$1;fi
+num=$(printf "%06d" $(echo $num + $nth| bc))
+echo $1
 echo SHOWING $num
 python 3dbox_to_img.py --file_id $num
